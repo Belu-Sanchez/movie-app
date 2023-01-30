e7055e2930c8c732eba956d5cdc1b24211f67758
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2023-01-30 17:40:27 -0300

    updatee/I created form forgot

--- src/components/forms/SignIn/index.tsx
+++ src/components/forms/SignIn/index.tsx
@@ -1,31 +1,38 @@
 import Form from 'react-bootstrap/Form';
+import { NavLink } from 'react-router-dom';
 import { Button } from '../index';
 import './styles.scss';
 
 const SignIn = () => {
-  return (
-    <>
-      <div className="container-sign-up"> 
-        <h3>Sign In</h3>
-        <Form>
+    return (
+        <>
+            <div className="container-sign-up">
+                <h3>Sign In</h3>
+                <Form>
 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email"  />
-          </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" />
+                    </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" />
-          </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" />
+                    </Form.Group>
 
 
-          <Button variant='' type='submit' >Submit</Button>
-        </Form>
-        </div>
+                    <Button variant='' type='submit' >Submit</Button>
 
-    </>
-  )
+                    <div>
+                        <NavLink to="/forgot">Forgot your password?</NavLink>
+
+                    </div>
+
+                </Form>
+            </div>
+
+        </>
+    )
 }
 
 export { SignIn };
--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,3 +1,4 @@
 export { SignUp as SignUpForm } from './SignUp';
 export { Button } from './Button';
-export { SignIn } from './SignIn'
+export { SignIn } from './SignIn';
+export { Forgot as ForgotForm } from './Forgot';
