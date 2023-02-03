dac97117ec53fd1741373806c65cf9e8c9b5f517
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2023-02-03 09:04:15 -0300

    update/pintando movies populares

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,32 @@
+import { FC, useEffect, useState } from "react";
+import { apiMovies, API_BASE_IMAGE } from "../../utils/axios";
+type Props = {
+    id:number,
+    title: String
+    poster_path: String
+}
 const Dashboard = () => {
+
+    const [movies, setMovies] = useState<Props[]>([])
+
+    useEffect(() => {
+        apiMovies.get('/movie/popular').then(response => setMovies(response.data.results))
+    },[])
+    console.log(movies)
     return (
-        <>
+        <> 
+   
         <div>Dashboard</div>
+        <div >
+            {
+            movies.map(movie => (<div><div key={movie.id}>{movie.title}</div>
+            <img src={`${API_BASE_IMAGE + movie.poster_path}`} ></img>
+            </div>
+            ))
+            
+            }
+        
+        </div>
         </>
     );
 }
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,15 +1,21 @@
 import axios from 'axios'
 
-export const API_BASE_URL = "https://api.themoviedb.org/3";
-export const API_KEY_URL = "0df0bd9988c9e9f56e2bc6d6b20adce2";
+// export const API_BASE_URL = "https://api.themoviedb.org/3";
+// export const API_KEY_URL = "0df0bd9988c9e9f56e2bc6d6b20adce2";
 export const API_BASE_IMAGE = "https://image.tmdb.org/t/p/original";
 
 
-
+// cambiar a apiUsers
 const api = axios.create({
     baseURL: 'https://movies-app-4872f-default-rtdb.firebaseio.com/'
 })
 
+const apiMovies = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    params: {
+        api_key :  "0df0bd9988c9e9f56e2bc6d6b20adce2"
+    }
+})
 
 
-export { api }
+export { api, apiMovies }
